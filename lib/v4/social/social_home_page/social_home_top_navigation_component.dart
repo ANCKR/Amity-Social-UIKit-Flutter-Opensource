b16043de9601a8118fb9b7c54527000f2b7526ee
Pragyan Deep Gogoi <pragyan.gagoi@xotiv.com>
2025-10-15 15:15:53 +0530

    refactor: Comment out post creation functionality in SocialHomeTopNavigationComponent to prevent user access while maintaining code structure

--- lib/v4/social/social_home_page/social_home_top_navigation_component.dart
+++ lib/v4/social/social_home_page/social_home_top_navigation_component.dart
@@ -1,8 +1,8 @@
 import 'package:amity_uikit_beta_service/v4/chat/home/chat_home_page.dart';
 import 'package:amity_uikit_beta_service/v4/core/amity_colors.dart';
 import 'package:amity_uikit_beta_service/v4/core/base_component.dart';
-import 'package:amity_uikit_beta_service/v4/social/community/community_creation/community_setup_page.dart';
-import 'package:amity_uikit_beta_service/v4/social/social_home_page/create_post_menu_component.dart';
+// import 'package:amity_uikit_beta_service/v4/social/community/community_creation/community_setup_page.dart';
+// import 'package:amity_uikit_beta_service/v4/social/social_home_page/create_post_menu_component.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 
@@ -95,33 +95,34 @@ class AmitySocialHomeTopNavigationComponent extends NewBaseComponent {
             );
           },
         ),
-        if (selectedTab == AmitySocialHomePageTab.newsFeed)
-          AmityCreatePostMenuComponent(),
-        if (selectedTab == AmitySocialHomePageTab.myCommunities)
-          GestureDetector(
-            onTap: () => Navigator.of(context).push(MaterialPageRoute(
-                fullscreenDialog: true,
-                builder: (context) =>
-                    AmityCommunitySetupPage(mode: const CreateMode()))),
-            child: Padding(
-                padding: const EdgeInsets.only(right: 16),
-                child: Container(
-                    width: 32,
-                    height: 32,
-                    decoration: const BoxDecoration(
-                      color: Colors.white,
-                      shape: BoxShape.circle,
-                    ),
-                    child: Center(
-                        child: SvgPicture.asset(
-                      "assets/Icons/amity_ic_post_creation_button.svg",
-                      package: 'amity_uikit_beta_service',
-                      colorFilter: ColorFilter.mode(
-                        theme.primaryColor,
-                        BlendMode.srcIn,
-                      ),
-                    )))),
-          ),
+        // Commented out: Users can't create posts
+        // if (selectedTab == AmitySocialHomePageTab.newsFeed)
+        //   AmityCreatePostMenuComponent(),
+        // if (selectedTab == AmitySocialHomePageTab.myCommunities)
+        //   GestureDetector(
+        //     onTap: () => Navigator.of(context).push(MaterialPageRoute(
+        //         fullscreenDialog: true,
+        //         builder: (context) =>
+        //             AmityCommunitySetupPage(mode: const CreateMode()))),
+        //     child: Padding(
+        //         padding: const EdgeInsets.only(right: 16),
+        //         child: Container(
+        //             width: 32,
+        //             height: 32,
+        //             decoration: const BoxDecoration(
+        //               color: Colors.white,
+        //               shape: BoxShape.circle,
+        //             ),
+        //             child: Center(
+        //                 child: SvgPicture.asset(
+        //               "assets/Icons/amity_ic_post_creation_button.svg",
+        //               package: 'amity_uikit_beta_service',
+        //               colorFilter: ColorFilter.mode(
+        //                 theme.primaryColor,
+        //                 BlendMode.srcIn,
+        //               ),
+        //             )))),
+        //   ),
       ],
       iconTheme: const IconThemeData(color: Colors.black),
     );
